95fe20b6f7b327bc2517378c0f0f0885e2bbafb8
Hassan Khan <hassankpisces@gmail.com>
2024-06-12 00:39:20 +0500

    currentNote undefined fix test 1

--- src/App.jsx
+++ src/App.jsx
@@ -39,7 +39,7 @@ export default function App() {
   }, [currentNote])
   React.useEffect(() => {
     const timeoutId = setTimeout(() => {
-      if (currentNote && tempNoteText !== currentNote.body) {
+      if (currentNote?.body && tempNoteText !== currentNote.body) {
         updateNote(tempNoteText)
       }
     }, 500)

33dfa5044391b19b7a33d73d18888450352e9fab
Hassan Khan <hassankpisces@gmail.com>
2024-06-27 08:32:25 +0500

    Collapsible fixed

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -28,11 +28,26 @@ export default function Sidebar(props) {
     return (
         <section className="pane sidebar">
             <div className="sidebar--header">
-                <button className="toggle-sidebar" onClick={toggleSidebarBody}>
-                    {isSidebarBodyVisible ? "Hide" : "Show"}
-                </button>
                 <h3>Notes</h3>
                 <button className="new-note" onClick={props.newNote}>+</button>
+                <button className="toggle-sidebar" onClick={toggleSidebarBody}>
+                    <svg
+                        xmlns="http://www.w3.org/2000/svg"
+                        width="24"
+                        height="24"
+                        viewBox="0 0 24 24"
+                        fill="none"
+                        stroke="currentColor"
+                        strokeWidth="2"
+                        strokeLinecap="round"
+                        strokeLinejoin="round"
+                        className="feather feather-menu"
+                    >
+                        <line x1="3" y1="12" x2="21" y2="12"></line>
+                        <line x1="3" y1="6" x2="21" y2="6"></line>
+                        <line x1="3" y1="18" x2="21" y2="18"></line>
+                    </svg>
+                </button>
             </div>
 
             <div className={`sidebar--body ${isSidebarBodyVisible ? "visible" : ""}`}>
